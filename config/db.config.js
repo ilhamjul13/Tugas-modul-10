5b8fd4c85660744fc1cb153be3eda6f8e0d1fc11
Ilhamjul <hamzul1302@gmail.com>
2023-10-23 21:59:55 +0700

    push modul 10

---
+++ config/db.config.js
@@ -0,0 +1,10 @@
+const dbConfig = {
+    HOST: 'localhost',
+    USER: 'root',
+    PASSWORD: 'Ilhamjul13#',
+    DATABASE: 'modul10',
+    DIALECT: 'mysql',
+    PORT: 3306
+};
+
+module.exports = dbConfig;
